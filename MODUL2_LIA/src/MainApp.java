ed8f3a64ae971aa39211eccd3d92cb03f692a54a
Liaagrn17 <liaanggreni@student.telkomuniversity.ac.id>
2022-12-05 08:54:44 +0700

    first commit

---
+++ MODUL2_LIA/src/MainApp.java
@@ -0,0 +1,21 @@
+public class MainApp {
+    public static void main(String[] args) throws Exception {
+        Kapal kapal1 = new Kapal("int jumlah kursi","int biaya", "String Mesin",);
+        kapal1.informasi();
+        System.out.println("");
+
+        Sampan 1 = new Sampan("Sampan", 1500, 160.000,true);
+        sampan1.informasi();
+        sampan1.berlayar("Sumatra");
+        sampan1.berlabuh("Jawa");
+        sampan1.Berlabuh("int jangkar");
+        System.out.println("");
+
+        Transportasiair TransportasiAir1 = new TransportasiAir("Ferry",1500,160.000, true);
+        TransportasiAir1.informasi();
+        TransportasiAir1.berlayar();
+        TransportasiAi11.berlabuh();
+        System.out.println("");
+    }
+}
+
